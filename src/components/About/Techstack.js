8afc3495cd5b3731838cf1bc61fa9f8014e8d203
Muhammad Umair <muhammadumair523@gmail.com>
2025-08-03 19:20:30 +0500

    added title prop

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -29,91 +29,91 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='JavaScript'>
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='TypeScript'>
         <SiTypescript />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Python'>
         <DiPython />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Java'>
         <DiJava />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='C++'>
         <CgCPlusPlus />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='React'>
         <DiReact />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Next.js'>
         <SiNextdotjs />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Redux'>
         <SiRedux />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Tailwind CSS'>
         <SiTailwindcss />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Node.js'>
         <DiNodejs />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Express'>
         <SiExpress />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='GraphQL'>
         <SiGraphql />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='MongoDB'>
         <DiMongodb />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='PostgreSQL'>
         <SiPostgresql />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='MySQL'>
         <SiMysql />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Firebase'>
         <SiFirebase />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Supabase'>
         <SiSupabase />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Expo'>
         <SiExpo />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Vercel'>
         <SiVercel />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Netlify'>
         <SiNetlify />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Railway'>
         <SiRailway />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Heroku'>
         <SiHeroku />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Jest'>
         <SiJest />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Playwright'>
         <SiPlaywright />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Jira'>
         <SiJira />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Figma'>
         <SiFigma />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Adobe XD'>
         <SiAdobexd />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Postman'>
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Git'>
         <DiGit />
       </Col>
     </Row>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -22,52 +22,52 @@ import {
 function Toolstack() {
   return (
     <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Mac OS'>
         <SiMacos />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Windows'>
         <SiWindows />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Linux'>
         <SiLinux />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Visual Studio Code'>
         <SiVisualstudiocode />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='GitHub'>
         <SiGithub />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='GitLab'>
         <SiGitlab />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Bitbucket'>
         <SiBitbucket />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Figma'>
         <SiFigma />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Adobe XD'>
         <SiAdobexd />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Postman'>
         <SiPostman />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Jira'>
         <SiJira />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Trello'>
         <SiTrello />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Slack'>
         <SiSlack />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Discord'>
         <SiDiscord />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Zoom'>
         <SiZoom />
       </Col>
-      <Col xs={4} md={2} className='tech-icons'>
+      <Col xs={4} md={2} className='tech-icons' title='Vercel'>
         <SiVercel />
       </Col>
     </Row>
