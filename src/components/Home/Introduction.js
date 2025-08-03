9a89a5e1fe85c5c92ab1be745f33f4b0a0e98a96
Muhammad Umair <muhammadumair523@gmail.com>
2025-08-03 15:21:34 +0500

    updated introduction

--- src/components/Home/Introduction.js
+++ src/components/Home/Introduction.js
@@ -16,32 +16,19 @@ function Introduction() {
               LET ME <span className='purple'> INTRODUCE </span> MYSELF
             </h1>
             <p className='home-about-body'>
-              I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️
+              I'm a passionate software developer who loves turning complex problems into simple, beautiful solutions.
+              My journey in technology began with curiosity and has evolved into a deep passion for creating impactful
+              software.
               <br />
-              <br />I am fluent in classics like
+              <br />I specialize in modern web and mobile development and am proficient in
               <i>
-                <b className='purple'> C++, Javascript and Go. </b>
+                <b className='purple'> JavaScript, TypeScript, React Native, React.js, Node.js. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className='purple'>Web Technologies and Products </b> and also in areas related to{' '}
-                <b className='purple'>Blockchain.</b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products with <b className='purple'>
-                Node.js
-              </b>{' '}
-              and
-              <i>
-                <b className='purple'> Modern Javascript Library and Frameworks</b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className='purple'> React.js and Next.js</b>
-              </i>
+              When I'm not coding, you'll find me exploring new technologies, contributing to open-source projects, or
+              sharing knowledge with the developer community. I believe in continuous learning and staying updated with
+              the latest industry trends.
             </p>
           </Col>
           <Col md={4} className='myAvtar'>
